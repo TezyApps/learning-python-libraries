34094c457f3368e991fd1720661264d97450c062
Tezy Apps <tezyapps@gmail.com>
2026-08-14 16:22:30 +1000

    Read from csv using pandas lib

--- src/libraries_introduction/__init__.py
+++ src/libraries_introduction/__init__.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 def __test_pandas_lib() -> None:
     print("Exploring pandas library")
+    data_set = pd.read_csv("resources/datasets/student-performance.csv")
+    print(type(data_set))
 
 def main() -> None:
     print("Hello from libraries-introduction!")
