c5300b8d75697743c9991c64bda284dccb3cc22b
Tezy Apps <tezyapps@gmail.com>
2026-08-15 12:55:49 +1000

    Optimizing solution for reverse list without built-in functions

--- scripts/reverse-list.py
+++ scripts/reverse-list.py
@@ -3,13 +3,11 @@
 print("Problem: Reverse a list without built-in function")
 original_list = [1,2,3,4,5]
 
-count = len(original_list) - 1
+count = len(original_list)
 
 result_list = []
-while count >= 0:
-    result_list.append(original_list[count])
+while count > 0:
     count -= 1
-else:
-    print("Soemthing went wrong")
+    result_list.append(original_list[count])
 
 print(original_list, result_list)
